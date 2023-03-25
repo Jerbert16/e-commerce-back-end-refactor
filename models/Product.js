8d4a18a56dffa82c230ffd044dcc24d80d967945
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-03-25 11:30:18 -0600

    defined columns in various models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,27 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    part: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    manufacturer: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    }
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,7 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    part_number: {
+      type: DataTypes.UUID,
+      defaultValue: UUIDV4,
+    },
+    is_instock: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
   },
+},
   {
     sequelize,
     timestamps: false,
